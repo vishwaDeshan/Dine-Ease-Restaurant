f0ee37d763f624a2822729f9822d3a5394ebf42c
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-05-17 07:24:47 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DineEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
