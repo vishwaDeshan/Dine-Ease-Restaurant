bdf1461b8ff8afbd0806c29d6d371381426b7f73
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-05-20 19:18:15 +0530

    add product view

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using DineEase.Data;
+using DineEase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DineEase.Controllers
+{
+	public class ProductController : Controller
+	{
+		private Repository<Product> products {  get; set; }
+		private Repository<Ingredient> ingredients {  get; set; }
+		private Repository<Category> categories {  get; set; }
+
+		public ProductController(ApplicationDbContext context)
+		{
+			products = new Repository<Product>(context);
+			categories = new Repository<Category>(context);
+			ingredients = new Repository<Ingredient>(context);
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await products.GetAllAsync());
+		}
+	}
+}
