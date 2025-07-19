9d601f0aea1837b447fd4205a069a91ba364dbf6
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-07-19 17:02:21 +0530

    add/edit function

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -22,5 +22,27 @@ namespace DineEase.Controllers
 		{
 			return View(await products.GetAllAsync());
 		}
+
+		public async Task<IActionResult> AddEdit(int id)
+		{
+			ViewBag.Ingredient = ingredients.GetAllAsync();
+			ViewBag.Category = categories.GetAllAsync();
+
+			if (id == 0)
+			{
+				ViewBag.Operation = "Add";
+				return View(new Product());
+			}
+			else
+			{
+				ViewBag.Operation = "Edit";
+				var product = await products.GetByIdAsync(id, new QueryOptions<Product> { Includes = "ProductIngredients.Ingredient" });
+				if (product == null)
+				{
+					return NotFound();
+				}
+				return View(product);
+			}
+		}
 	}
 }
